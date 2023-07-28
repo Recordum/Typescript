01eaacc33f14a3cb74d5bdd89e0e90525c96fcac
Recordum <mingyu.oh15b@gmail.com>
2023-07-28 19:02:12 +0900

    [Datastructure]: LinkedList find Test 추가 및 append/prepend test 수정

--- src/Datastructure/List/LinkedList/LnkedList.spec.ts
+++ src/Datastructure/List/LinkedList/LnkedList.spec.ts
@@ -6,8 +6,6 @@ describe("LinkedList", () => {
 
   beforeEach(() => {
     linkedList = new LinkedList();
-    expect(linkedList.head).toBeNull();
-    expect(linkedList.tail).toBeNull();
   });
 
   test("prepend: LinkedList haed 에 추가", () => {
@@ -16,19 +14,28 @@ describe("LinkedList", () => {
     linkedList.prepend(3);
 
     expect(linkedList.size()).toBe(3);
-    expect(linkedList.head?.getValue()).toBe(3);
-    expect(linkedList.head?.next?.getValue()).toBe(2);
-    expect(linkedList.tail?.getValue()).toBe(1);
+    expect(linkedList.find(0)).toBe(3);
+    expect(linkedList.find(1)).toBe(2);
+    expect(linkedList.find(2)).toBe(1);
   });
 
-  test("append: linkedList tail 에 추가", () => {
+  test("append: LinkedList tail 에 추가", () => {
     linkedList.append(1);
     linkedList.append(2);
     linkedList.append(3);
 
     expect(linkedList.size()).toBe(3);
-    expect(linkedList.head?.getValue()).toBe(1);
-    expect(linkedList.head?.next?.getValue()).toBe(2);
-    expect(linkedList.tail?.getValue()).toBe(3);
-  })
+    expect(linkedList.find(0)).toBe(1);
+    expect(linkedList.find(1)).toBe(2);
+    expect(linkedList.find(2)).toBe(3);
+  });
+
+  test("find: 범위에 벗어난 index 접근시 Index Out Of Bound Error 발생", () => {
+    linkedList.append(0);
+    linkedList.prepend(1);
+
+    expect(linkedList.size()).toBe(2);
+    expect(() => linkedList.find(2)).toThrow("Index Out of Bound");
+    expect(() => linkedList.find(3)).toThrow("Index Out of Bound");
+  });
 }); 
