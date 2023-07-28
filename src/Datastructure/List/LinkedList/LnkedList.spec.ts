bc65dd17027cf46c1b0b70497340bdde6c18e626
Recordum <mingyu.oh15b@gmail.com>
2023-07-28 18:34:36 +0900

    [Datastructure]: LinkedList append/prepend test 추가

--- src/Datastructure/List/LinkedList/LnkedList.spec.ts
+++ src/Datastructure/List/LinkedList/LnkedList.spec.ts
@@ -2,15 +2,33 @@
 import { LinkedList } from "./linkedlist";
 
 describe("LinkedList", () => {
-  let linkedList: LinkedList;
+  let linkedList: LinkedList<number>;
 
   beforeEach(() => {
     linkedList = new LinkedList();
+    expect(linkedList.head).toBeNull();
+    expect(linkedList.tail).toBeNull();
   });
 
-  test("should return'LinkedList!'", () => {
-    const result = linkedList.hello();
+  test("prepend: LinkedList haed 에 추가", () => {
+    linkedList.prepend(1);
+    linkedList.prepend(2);
+    linkedList.prepend(3);
 
-    expect(result).toBe("LinkedList!");
+    expect(linkedList.size()).toBe(3);
+    expect(linkedList.head?.getValue()).toBe(3);
+    expect(linkedList.head?.next?.getValue()).toBe(2);
+    expect(linkedList.tail?.getValue()).toBe(1);
   });
-});
+
+  test("append: linkedList tail 에 추가", () => {
+    linkedList.append(1);
+    linkedList.append(2);
+    linkedList.append(3);
+
+    expect(linkedList.size()).toBe(3);
+    expect(linkedList.head?.getValue()).toBe(1);
+    expect(linkedList.head?.next?.getValue()).toBe(2);
+    expect(linkedList.tail?.getValue()).toBe(3);
+  })
+}); 
