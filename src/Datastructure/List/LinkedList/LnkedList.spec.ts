92ad2b0dd9788a395259bfaa7079bf008713fdeb
Recordum <mingyu.oh15b@gmail.com>
2023-07-28 19:19:20 +0900

    [Datastructure]: LinkedList IndexOf 테스트 코드 추가

--- src/Datastructure/List/LinkedList/LnkedList.spec.ts
+++ src/Datastructure/List/LinkedList/LnkedList.spec.ts
@@ -1,11 +1,11 @@
-// linkedlist.test.ts
-import { LinkedList } from "./linkedlist";
+import { LinkedList } from './linkedlist';
 
 describe("LinkedList", () => {
   let linkedList: LinkedList<number>;
-
+  let linkedListString: LinkedList<string>;
   beforeEach(() => {
     linkedList = new LinkedList();
+    linkedListString = new LinkedList();
   });
 
   test("prepend: LinkedList haed 에 추가", () => {
@@ -38,4 +38,26 @@ describe("LinkedList", () => {
     expect(() => linkedList.find(2)).toThrow("Index Out of Bound");
     expect(() => linkedList.find(3)).toThrow("Index Out of Bound");
   });
+
+  describe("indexOf Test", () => {
+    test("indexOf: List 에서 value 랑 일치하는 index 반환", () => {
+      linkedListString.append("Node0");
+      linkedListString.append("Node1");
+      linkedListString.append("Node2");
+
+      expect(linkedListString.size()).toBe(3);
+      expect(linkedListString.IndexOf("Node0")).toBe(0);
+      expect(linkedListString.IndexOf("Node1")).toBe(1);
+      expect(linkedListString.IndexOf("Node2")).toBe(2);
+    })
+    test("indexOf: List 에 없는 value 일 경우 Index of your value not found Error 발생", () =>{
+      linkedListString.append("Node0");
+      linkedListString.append("Node1");
+      linkedListString.append("Node2");
+
+      expect(linkedListString.size()).toBe(3);
+      expect(()=>linkedListString.IndexOf("Node3")).toThrow("Index of your value not found");
+    })
+  })
+  
 }); 
